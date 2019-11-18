7ac368847d234b29fed064b2027e31d42e51f13d
EduardoGomes18 <eduardo_gomes1318@outlook.com>
2019-11-18 01:57:22 -0300

    adicionado nova branch e arquivos para o site

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+var slideItem = 0;
+window.onload = function() {
+	setInterval(passarSlide, 2000);
+
+	var slidewidth = document.getElementById("slideshow").offsetWidth;
+	var objs = document.getElementsByClassName("slide");
+	for(var i=0;i<objs.length;i++) {
+		objs[i].style.width = slidewidth+"px";
+	}
+}
+function passarSlide() {
+	var slidewidth = document.getElementById("slideshow").offsetWidth;
+	
+	if(slideItem >= 3) {
+		slideItem = 0;
+	} else {
+		slideItem++;
+	}
+
+	document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+}
+function mudarSlide(pos) {
+	slideItem = pos;
+	var slidewidth = document.getElementById("slideshow").offsetWidth;
+	document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+	var selectedslide = document.getElementById("slideshow");
+	document.getElementByClassName ("balls") [0].style.opacity=0.3;
+}
+
+
+
+
+// function slideselect(pos){
+// 	slideItem=pos;
+// 	var selectedslide = document.getElementById("slideshow");
+// 	document.getElementByClassName ("balls") [0].style.opacity=0.3;
+// }
+
+
+
+
+function toggleMenu() {
+
+	var menu = document.getElementById("menu");
+
+	if (menu.style.display == 'none' || menu.style.display == '') {
+		menu.style.display = "block";
+	} else {
+		menu.style.display = "none";
+	}
+
+}
+
